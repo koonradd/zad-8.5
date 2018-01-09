04cef05b7be91471731f9bb433f44d1823461b9a
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-01-10 00:49:54 +0100

    initial

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var a = prompt('Podaj wartosc a', a);
+var h = prompt('Podaj wartosc h', h);
+
+var triangleArea = a * h / 2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
